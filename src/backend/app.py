884e93919180b56101c56b14f7bce3c0fa64faf9
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-11-13 18:28:11 +0530

    Initialize FastAPI app with root endpoint

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,1 +1,7 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Cloud-based Digital Twin Generator for Healthcare Prediction using Generative AI"}
